2623a4749135c0cebc9c22c2e49b3e935a49ce97
Nupur <nrmcodes22@gmail.com>
2024-03-09 19:27:35 +0530

    ts files new and images

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+
+import {SparklesPreview} from '../components/sparkles-comp'
+import {BentoGridDemo} from '../components/bento-comp'
+import {AnimatedTooltipPreview} from '../components/tooltip-comp'
+import {BackgroundGradientDemo} from '../components/bg-gradient-comp'
+import {SparklesPreviewFooter} from '../components/footer'
+export default function Home() {
+  return (
+    <section >
+      <SparklesPreview/>
+      <BentoGridDemo/>
+      <AnimatedTooltipPreview/>
+      <BackgroundGradientDemo/>
+      <SparklesPreviewFooter/>
+    </section>
+  );
+}
---
+++ src/components/bg-gradient-comp.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React from "react";
+import { BackgroundGradient } from "../components/ui/background-gradient";
+import { IconAppWindow } from "@tabler/icons-react";
+import Image from "next/image";
+const handleClick = () => {
+  // Your click event handling logic here
+  window.location.href = "https://www.hackodisha.tech/";
+}
+export function BackgroundGradientDemo() {
+  return (
+    <>
+    <h2 className="md:text-7xl text-3xl lg:text-7xl font-bold text-center text-white relative z-20 mt-16 mb-16">
+        Flagship Event
+      </h2>
+    <div className="flex justify-center">
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`/image/HO.png`}
+          alt="jordans"
+          height="400"
+          width="400"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+          HACKODISHA
+        </p>
+
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+        Hackodisha, the premier tech event of NIT ROURKELA, brings together innovators, developers, and enthusiasts from around the globe for a transformative journey into the realms of technology and creativity. Join us as we explore cutting-edge solutions, foster collaboration, and push the boundaries of innovation.
+        </p>
+        <button className="rounded-full pl-2 pr-2 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800 " onClick={handleClick}>
+          <span>Website</span>
+          
+        </button>
+      </BackgroundGradient>
+    </div>
+    </>
+  );
+}
