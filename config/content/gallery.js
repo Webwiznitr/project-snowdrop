3e61adf3b27734c45b8633d35f20ab4c9ed58393
SpryzenAd <adhishsahu001@gmail.com>
2024-05-09 23:39:56 +0530

    changed config images

--- config/content/gallery.js
+++ config/content/gallery.js
@@ -1,20 +1,7 @@
 export const logos = [
-  // NIT Rourkela
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ-Bz6QS-bSezCgILrZho_qg0MGP_KgcB5y289ECFg_GEcsim7o56Ku3KVKQ44dusESVM&usqp=CAU",
-  // Indian Oil
-  "https://media.gettyimages.com/id/1399150019/photo/abstract-network-and-data-speed.jpg?s=612x612&w=gi&k=20&c=V6MOs0_2sk6uF5MEd3IhTosbgaomfFbsbbVHaVyq85U=",
-  // Railways
-  "https://res.cloudinary.com/dq9gdlbls/image/upload/f_auto,q_auto/t0wa1rxfuapm0wv53ird",
-  // Tata Steel
-  "https://cdn.ces.tech/ces/media/articles/2022/october/innovation.jpg",
-  // Indian Navy
-  "https://res.cloudinary.com/dgjzygzgx/image/upload/v1705699568/Indian_Navy-removebg-preview_1_scsylv.png",
-  // Hindalco
-  "https://res.cloudinary.com/dgjzygzgx/image/upload/v1705691398/Hindalco-removebg-preview_zyc9dj.png",
-  // SAIL
-  "https://res.cloudinary.com/dq9gdlbls/image/upload/f_auto,q_auto/lcgsrqq3ljwjqxq1tu66",
-  // NINL
-  "https://res.cloudinary.com/dgjzygzgx/image/upload/v1705691398/NINL-removebg-preview_sxojzn.png",
-  // iDex
-  "https://www.simplilearn.com/ice9/free_resources_article_thumb/Technology_Trends.jpg",
+  "/image/anushrey.jpg",
+  "/image/raj.jpg",
+  "/image/ankit.jpg",
+  "/image/anushrey.jpg",
+  "/image/raj.jpg",
 ];
