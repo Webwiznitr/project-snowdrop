e9e69ff9f2292f86d9ed138ede17591cf5ac8d74
SpryzenAd <adhishsahu001@gmail.com>
2024-05-09 16:55:19 +0530

    added carousel and padding

--- src/app/spryzen/page.tsx
+++ src/app/spryzen/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import EventsPage from './events';
 import Team from './team';
--- src/app/spryzen/team.tsx
+++ src/app/spryzen/team.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
+
 
 const teamMembers = [
     {
@@ -11,13 +15,13 @@ const teamMembers = [
     {
         id: 2,
         name: "Name",
-        image: "/image/anushrey.jpg",
+        image: "/image/raj.jpg",
         role: "Role",
     },
     {
         id: 3,
         name: "Name",
-        image: "/image/anushrey.jpg",
+        image: "/image/ankit.jpg",
         role: "Role",
     },
     {
@@ -29,10 +33,24 @@ const teamMembers = [
     {
         id: 5,
         name: "Name",
-        image: "/image/anushrey.jpg",
+        image: "/image/raj.jpg",
         role: "Role",
     },
 ];
+
+const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 600,
+    fade: true,
+    autoplaySpeed: 7000,
+    pauseOnHover: true,
+    swipeToSlide: true,
+    cssEase: "linear",
+  };
 
 export default function Team(){
     return(
@@ -44,18 +62,30 @@ export default function Team(){
         bg-gradient-to-r from-[#8C6CEA] via-[#FF5E69] to-[#FFA84B]">
             Meet our Team
         </h2>
-        <p className="mx-auto mt-2 max-w-[550px]">Just take a look - each member of the team is watching your
+        <p className="mx-auto px-4 mt-2 max-w-[550px]">Just take a look - each member of the team is watching your
             every gesture and will hear your every whisper.
         </p>
         </div>
 
-        <div className="flex flex-wrap justify-evenly mt-7 mx-auto w-[90%] space-x-3">
+        <div className="hidden sm:flex flex-wrap justify-evenly mt-7 mx-auto w-[90%] space-x-3">
             {teamMembers.map((member) => 
                 <div key={member.id} className="mb-7">
                     <Image src={member.image} alt="" width={224} height={467}
                     className="w-[224px] h-[467px] object-cover rounded-lg" />
                 </div>
             )}
+        </div>
+        
+        <div className="sm:hidden w-3/4 mx-auto py-10 ">
+        <Slider {...settings}>
+            {teamMembers.map((member) => 
+                <div key={member.id} className="flex">
+                    <Image src={member.image} alt="" width={224} height={467}
+                    className="w-[224px] h-[467px] object-cover rounded-lg
+                    mx-auto" />
+                </div>
+            )}
+        </Slider>
         </div>
         </>
     );
