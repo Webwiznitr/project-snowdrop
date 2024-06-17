642e713854b703fed6bdd9ae7cc614d78cbca8de
narashimha05 <chinnarinarashimha@gmail.com>
2024-06-17 14:05:33 +0530

    changed Circle Image

--- src/app/Circle/page.tsx
+++ src/app/Circle/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Circle() {
+export default function page() {
   return (
     <div className="w-full h-screen">
       <Image src={"/Group.svg"} alt="webwizcircle" fill={true}/>
